8e4c85dedbc4a20439fbc7b8d0814d50ec89bc4d
Moahmmad Rakib <mohammadrakib224@gmail.com>
2025-08-23 20:03:49 +0600

    Get started activity getstarted button functionality implemented

--- app/src/main/java/com/example/meditrack/GetStartedActivity.kt
+++ app/src/main/java/com/example/meditrack/GetStartedActivity.kt
@@ -1,20 +1,26 @@
 package com.example.meditrack
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.meditrack.databinding.ActivityGetstartedBinding
 
 class GetStartedActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGetstartedBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_getstarted)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.get_started_layout)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityGetstartedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonGetStarted.setOnClickListener {
+            val intent = Intent(this, Login_And_Signup_Activity::class.java)
+            startActivity(intent)
         }
     }
 }
