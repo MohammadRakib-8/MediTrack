5aa6404db61e32266e2e62b52a639c72e7bebddc
Moahmmad Rakib <mohammadrakib224@gmail.com>
2025-08-24 10:48:22 +0600

    UI updated

--- app/src/main/java/com/example/meditrack/GetStartedActivity.kt
+++ app/src/main/java/com/example/meditrack/GetStartedActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.meditrack.databinding.ActivityGetstartedBinding
@@ -14,10 +15,17 @@ class GetStartedActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        setTheme(R.style.Theme_MediTrack);
+        //for splashscreen scope
+        Thread.sleep(1000)
+        installSplashScreen()
 
         enableEdgeToEdge()
 
         binding = ActivityGetstartedBinding.inflate(layoutInflater)
+
+
+
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.getStartedLayout) { v, insets ->
